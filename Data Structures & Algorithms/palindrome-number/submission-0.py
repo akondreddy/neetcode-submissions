82661aa315f9f4006e0249fe91d4b2702936cefd
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-08-01 09:41:37 -0400

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reverse = 0
+        temp = x
+        while temp > 0:
+            reverse = reverse * 10 + temp % 10
+            temp = temp // 10
+        return x == reverse
