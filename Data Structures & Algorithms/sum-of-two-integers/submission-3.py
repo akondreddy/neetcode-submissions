00cd7a6365df2d01c603f61260119b1b496c8fe5
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-08-03 14:35:35 -0400

    Add: sum-of-two-integers - submission-3

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # Use the mask to restrict Python to 32 bits
+        mask = 0xFFFFFFFF
+        # This is the max positive signed integer
+        max_int = 0x7FFFFFFF
+        while b != 0:
+            carry = (a & b) << 1
+            # Sum without the carry
+            a = (a ^ b) & mask
+            # Move the carry into b
+            b = carry & mask
+        return a if a <= max_int else ~(a ^ mask)
