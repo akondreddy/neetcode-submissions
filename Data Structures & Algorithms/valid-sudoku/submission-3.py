c12c7b69e977801ec10d6e736e6b7a47fc6101c5
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-09-09 18:34:47 -0400

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,50 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Brute force
+        # Three independent checks that will show
+        # if in the row, col, or square
+
+        for row in range(9):
+            rowSet = set()
+            for cell in range(9):
+                value = board[row][cell]
+                if value == ".":
+                    continue
+                if value in rowSet:
+                    return False
+                else:
+                    rowSet.add(value)
+        
+        for col in range(9):
+            colSet = set()
+            for cell in range(9):
+                value = board[cell][col]
+                if value == ".":
+                    continue
+                if value in colSet:
+                    return False
+                else:
+                    colSet.add(value)
+
+        
+        for square in range(9):
+            squareSet = set()
+            for row in range(3):
+                for col in range(3):
+                    # This logic determines which box
+                    # Row uses floor division
+                    # to index the row slowly.
+                    # Col uses modulo to iterate
+                    # faster.
+                    curRow = (square // 3) * 3 + row
+                    curCol = (square % 3) * 3 + col
+                    
+                    value = board[curRow][curCol]
+                    if value == ".":
+                        continue
+                    if value in squareSet:
+                        return False
+                    else:
+                        squareSet.add(value)
+
+        return True
