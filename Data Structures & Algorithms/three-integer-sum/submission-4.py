c46c8358f126871975adbb267b7be5e47fbd5601
Ashwin Kondreddy <ashwin.kondreddy@gmail.com>
2026-09-10 22:37:20 -0400

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        length = len(nums)
+        results = []
+
+        # Idea is to what we did in 2Sum, just again
+        # Reminder to sum to 0
+        for i in range(length):
+            seen = set()
+            first = nums[i]
+            for j in range(i + 1, length):
+                second = nums[j]
+                leftover = -1 * (first + second)
+                
+                if leftover in seen:
+                    triple = [first, second, leftover]
+                    triple = sorted(triple)
+                    if triple not in results:
+                        results.append(triple)
+                
+                seen.add(second)
+
+        return results
